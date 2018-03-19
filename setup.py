f45c76fa0fcb6eca88ccb1bccfe5a98c17001475
Juan Medina <juansantiago.medina@gmail.com>
2018-03-19 18:31:05 -0400

    👕 add classifiers

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
-#!/usr/bin/env python
+"""disambiguate setup.py."""
+
 from setuptools import setup
 
-version = "1.0"
 
-setup(name='disambiguate',
-      version=version,
-      description='Script to disambiguate reads mapping to multiple genomes.',
-      author='Miika Ahdesmaki',
-      license="MIT",
-      install_requires = [
-          'pysam>=0.8.4',
-          ],
-      py_modules = [
-          'disambiguate',
-          ],
-      entry_points = {
-          'console_scripts': [
-              'disambiguate = disambiguate:main',
-              ],
-          },
-      )
+setup(
+    name="disambiguate",
+    version="1.0",
+    description="Script to disambiguate reads mapping to multiple genomes.",
+    author="Miika Ahdesmaki",
+    license="MIT",
+    install_requires=["pysam>=0.8.4"],
+    py_modules=["disambiguate"],
+    entry_points={
+        "console_scripts": [
+            "disambiguate=disambiguate:main",
+            ],
+        },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2 :: Only",
+        "Topic :: Utilities"
+        ],
+    )
